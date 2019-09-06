78537a198632be90daa0d803c98dd3c79de78c06
dpinchuk <pinchuk.dap@gmail.com>
2019-09-06 14:29:35 +0300

    code refactoring

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -11,30 +11,26 @@ public abstract class BaseTest {
 
     @BeforeMethod
     public void setUp() {
-
         /*
         Every Testing scenario should be executed on some specific testing environment. The testing environment can be a web browser, Mobile device, mobile emulator, mobile simulator, etc.
 
         The Desired Capabilities Class helps us to tell the webdriver, which environment we are going to use in our test script.
          */
         PropertiesReader propertiesReader = new PropertiesReader();
-
         DesiredCapabilities caps = new DesiredCapabilities();
         caps.setJavascriptEnabled(true); //We allow execution of javaScript
         System.setProperty("webdriver.chrome.driver", propertiesReader.getChromeDriver());
         this.driver = new ChromeDriver();
         this.driver.manage().deleteAllCookies();
         this.driver.manage().window().maximize();
-
     }
 
     @AfterMethod
     public void tearDown() {
         if (this.driver != null) {
             this.driver.manage().deleteAllCookies();
-            //this.driver.quit();
+            this.driver.quit();
         }
-
     }
 
     public WebDriver getDriver() {
@@ -42,4 +38,3 @@ public abstract class BaseTest {
     }
 
 }
-
